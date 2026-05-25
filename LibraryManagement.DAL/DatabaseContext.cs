0e0d475523893e9de9b5731af3ac581f9c55e0fa
NTLnee <linh43807@gmail.com>
2026-05-26 00:52:04 +0700

    Update DatabaseContext.cs

--- LibraryManagement.DAL/DatabaseContext.cs
+++ LibraryManagement.DAL/DatabaseContext.cs
@@ -1,7 +1,32 @@
+using System.Data;
+using Microsoft.Data.SqlClient;
+
 namespace LibraryManagement.DAL
 {
-    public class DatabaseContext
+    public class DatabaseContext : IDisposable
     {
-        // DbSet<Sach>, DbSet<DocGia>...
+        private SqlConnection _connection;
+
+        public DatabaseContext()
+        {
+            _connection = ConnectionHelper.GetConnection();
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                if (_connection.State == ConnectionState.Closed)
+                    _connection.Open();
+                return _connection;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_connection.State == ConnectionState.Open)
+                _connection.Close();
+            _connection.Dispose();
+        }
     }
 }
