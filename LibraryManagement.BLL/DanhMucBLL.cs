25a807e9755b6440ec49d35e9e84e1bf00b709c1
NTLnee <linh43807@gmail.com>
2026-05-26 00:54:31 +0700

    Update DanhMucBLL.cs

--- LibraryManagement.BLL/DanhMucBLL.cs
+++ LibraryManagement.BLL/DanhMucBLL.cs
@@ -1,6 +1,16 @@
+using LibraryManagement.DAL.Repositories;
+using LibraryManagement.Models;
+
 namespace LibraryManagement.BLL
 {
     public class DanhMucBLL
     {
+        private readonly DanhMucRepository _repo = new();
+
+        public IEnumerable<DanhMuc> GetAll() => _repo.GetAll();
+        public IEnumerable<DanhMuc> GetByLoai(string loai) => _repo.GetByLoai(loai);
+        public void Add(DanhMuc dm) => _repo.Add(dm);
+        public void Update(DanhMuc dm) => _repo.Update(dm);
+        public void Delete(string id) => _repo.Delete(id);
     }
 }
