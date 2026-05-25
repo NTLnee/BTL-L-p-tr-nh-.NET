65154231bf7851795f6edf95c2a3dc03a60457b2
NTLnee <linh43807@gmail.com>
2026-05-26 00:51:06 +0700

    Update ConnectionHelper.cs

--- LibraryManagement.DAL/ConnectionHelper.cs
+++ LibraryManagement.DAL/ConnectionHelper.cs
@@ -1,7 +1,22 @@
+using Microsoft.Data.SqlClient;
+
 namespace LibraryManagement.DAL
 {
     public static class ConnectionHelper
     {
-        // Quản lý chuỗi kết nối
+        // Sửa thông tin server/database cho đúng máy của nhóm
+        private static readonly string _connectionString =
+            "Server=.;Database=LibraryManagement;Trusted_Connection=True;" +
+            "TrustServerCertificate=True;";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+
+        public static string GetConnectionString()
+        {
+            return _connectionString;
+        }
     }
 }
